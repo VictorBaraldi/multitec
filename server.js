aee2074036ac256089e9f66b97769d1d3ce49aec
VictorBaraldi <83473814+VictorBaraldi@users.noreply.github.com>
2025-11-03 08:34:28 -0300

    Update server.js

--- server.js
+++ server.js
@@ -29,6 +29,13 @@ const corsOptions = {
 app.use(cors(corsOptions));
 app.use(express.json());
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'http://127.0.0.1:5500');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
+
 const dbConfig = {
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
@@ -744,4 +751,5 @@ async function startServer() {
 }
 
 startServer();
+
 
